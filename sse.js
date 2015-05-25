6ebb3c7220d1a543b2259b02edd91141b1542fc1
digital planets <success@dpsw.info>
2015-05-25 11:21:40 -0500

    Update sse.js

--- sse.js
+++ sse.js
@@ -5,23 +5,23 @@ module.exports = {
   handle: function(r, s){
     s.setHeader('Content-Type', 'text/event-stream');
     s.setHeader('Access-Control-Allow-Origin', '*');
-    clients[r.cookies && r.cookies.t || Date.now()] = s;
+    var id = (r.cookies && r.cookies.t) || Date.now();
+    clients[id] = s;
+    r.on('close', function(){
+      delete clients[id];
+    });
   },
 
-  send: function(cli, msg){
-    if(typeof msg !== 'string')
-      msg = JSON.stringify(msg);
-    var c = clients;
-
-    if(!msg){
-      if(typeof cli !== 'string')
-        cli = JSON.stringify(cli);
-      for(i in c){
-        c[i].write('data:'+cli+'\n\n'); c[i].end();
-  	  }
-  	  return;
-    }
-    c[cli] && c[cli].write('data:'+msg+'\n\n') && c[cli].end();
+  send: function(msg){
+      if(typeof msg !== 'string') msg = JSON.stringify(msg);
+      for(i in clients){
+        clients[i].write('data:'+msg+'\n\n');
+  	  }    
+  },
+  
+  sendTo: function(cli, msg){
+    if(typeof msg !== 'string') msg = JSON.stringify(msg);
+    clients[cli].write('data:'+msg+'\n\n');
   }
-
+  
 }
